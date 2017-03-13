7b55e012b3792861b4361e97927975db3415898c
mindziukasss <mindziukasss@gmail.com>
2017-03-13 15:39:58 +0200

    Added js from validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,6 +1,10 @@
 @extends('main')
 
-@section('title', '| Create New Post')
+@section('title','| Create New Post')
+
+@section('stylesheets')
+  {!! Html::style('css.parsley.css') !!}
+@endsection
 
 @section('content')
 
@@ -8,12 +12,12 @@
     <div class="col-md-8 col-md-offset-2">
       <h1>Create New Post</h1>
       <hr>
-      {!! Form::open(['route' => 'posts.store']) !!}
+      {!! Form::open(['route' => 'posts.store', 'data-parsley-validate' => '']) !!}
         {{ Form::label('title', 'Title:') }}
-        {{ Form::text('title', null, array('class' => 'form-control')) }}
+        {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
 
         {{ Form::label('body', "Post:") }}
-        {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+        {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
 
         {{ Form::submit('Create Post', array('class' => 'btn btn-success', 'style' => 'margin-top: 20px;' )) }}
 
@@ -21,6 +25,13 @@
     </div>
   </div>
 
+@endsection
+@section('stylesheets')
+  {!! Html::style('css/parsley.css') !!}
+@endsection
 
+@section('scripts')
+
+  {!! Html::script('js/parsley.min.js') !!}
 
 @endsection
