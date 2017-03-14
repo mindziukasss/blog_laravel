8a48b0eab603acad2cd1c5905f1b42ff8cb340f1
mindziukasss <mindziukasss@gmail.com>
2017-03-14 10:43:12 +0200

    Added posts date

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,35 @@
 
 @section('content')
 
-  <h1>{{ $post->title }}</h1>
-  <p class="lead">{{ $post->body }}</p>
+  <div class="row">
+    <div class="col-md-8">
+      <h1>{{ $post->title }}</h1>
+      <p class="lead">{{ $post->body }}</p>
+    </div>
+
+    <div class="col-md-4">
+      <div class="well">
+        <dl class="dl-horizontal">
+          <dt>Create At:</dt>
+          <dd>{{ date('M j, Y H:i', strtotime($post->created_at)) }}</dd>
+        </dl>
+
+        <dl class="dl-horizontal">
+          <dt>Last Updated:</dt>
+          <dd>{{ date('M j, Y H:i', strtotime( $post->updated_at)) }}</dd>
+        </dl>
+        <hr>
+        <div class="row">
+          <div class="col-md-6">
+            <a href="#" class="btn btn-primary btn-block">Edit</a>
+          </div>
+
+          <div class="col-md-6">
+            <a href="#" class="btn btn-danger btn-block">Delete</a>
+          </div>
+      </div>
+    </div>
+  </div>
+
+
 @endsection
