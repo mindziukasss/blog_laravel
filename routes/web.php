93831a8d6ef819624836c601bb8207477ececab0
mindziukasss <mindziukasss@gmail.com>
2017-03-21 12:37:48 +0200

    Added comments

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::resource('categories', 'CategoryController', ['except' => ['create']]);
 Route::resource('tags', 'TagController', ['except' => ['create']]);
 
+Route::post('comments/{post_id}', ['uses' => 'CommentsController@store', 'as' => 'comments.store']);
+
 
 Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
