da138b9965321e0fbc48962c0dfbd010aa3a4b11
mindziukasss <mindziukasss@gmail.com>
2017-03-21 15:00:19 +0200

    Comments styling

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -21,10 +21,21 @@
 
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
+      <h3 class="comments-title"><span class="glyphicon glyphicon-comment"></span>{{ $post->comments()->count() }} Comments</h3>
       @foreach($post->comments as $comment)
         <div class="comment">
-          <p><strong>Name:</strong> {{ $comment->name }}</p>
-          <p><strong>Comment:</strong><br/>{{ $comment->comment }}</p><br>
+          <div class="author-info"> 
+          <img src="{{ "https://www.gravatar.com/avatar/" . md5(strtolower(trim($comment->email))) . "?s=50&d=mm" }}" class="author-image">
+            <div class="author-name"> 
+              <h4>{{ $comment->name }}</h4>
+              <p id="author-time">
+                {{ date('F nS, Y - G:i',strtotime($comment->created_at)) }}
+              </p>
+            </div>
+          </div>
+          <div class="comment-content"> 
+            {{ $comment->comment }}
+          </div>
         </div>
       @endforeach
     </div>
@@ -49,7 +60,7 @@
             {{ Form::label('comment', "Comment:") }}
             {{ Form::textarea('comment', null, ['class' => 'form-control', 'rows' => '5']) }}
 
-            {{ Form::submit('Add Comment', ['class' => 'btn btn-success btn-block', 'style' => 'margin-top:15px;']) }}
+            {{ Form::submit('Add Comment', ['class' => 'btn btn-success', 'style' => 'margin-top:15px;']) }}
           </div>
         </div>
 
