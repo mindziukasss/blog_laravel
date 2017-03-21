2939a3b457c4eacea94077da7716afe694990f71
mindziukasss <mindziukasss@gmail.com>
2017-03-21 10:19:46 +0200

    Added sending email from contact form

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,8 +1,11 @@
 <?php 
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
+use App\Http\Requests;
 use App\Post;
+use Mail;
+use Session;
 
 class PagesController extends Controller {
 
@@ -27,4 +30,27 @@ class PagesController extends Controller {
     return view('pages.contact');
   }
 
+  public function postContact(Request $request){
+    $this->validate($request, [
+      'email' => 'required|email',
+      'subject' => 'min:3',
+      'message' => 'min:10'
+      ]);
+
+    $data = array(
+      'email' => $request->email,
+      'subject' => $request->subject,
+      'bodyMessage' => $request->message
+      );
+
+    Mail::send('emails.contact', $data, function($message) use ($data){
+      $message->from($data['email']);
+      $message->to('mindziukass@gmail.com');
+      $message->subject($data['subject']);
+
+    });
+
+    Session::flash('success' , 'Your Email was Sent!');
+    return redirect('/');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::resource('tags', 'TagController', ['except' => ['create']]);
 Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
 Route::get('contact', 'PagesController@getContact');
+Route::post('contact', 'PagesController@postContact');
 Route::get('about', 'PagesController@getAbout');
 Route::get('/', 'PagesController@getIndex');
 Route::resource('posts', 'PostController');
