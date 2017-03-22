c9f5b3f0cf218d1edf1d543a1a81f48030065d75
mindziukasss <mindziukasss@gmail.com>
2017-03-22 08:35:29 +0200

    Added managing comments

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -10,32 +10,12 @@ use Session;
 
 class CommentsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+   
+    public function __construct()
     {
-        //
+        $this->middleware('auth', ['except' => 'store']);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request, $post_id)
     {
          $this->validate($request, array(
@@ -66,20 +46,12 @@ class CommentsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
-        //
+        $comment = Comment::find($id);
+        return view('comments.edit')->withComment($comment);
+
     }
 
     /**
@@ -91,7 +63,23 @@ class CommentsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $comment = Comment::find($id);
+        $this->validate($request, array('comment' => 'required'));
+
+        $comment->comment = $request->comment;
+        $comment->save();
+
+        Session::flash('success', 'Comment update');
+
+        return redirect()->route('posts.show', $comment->post->id);
+
+    }
+
+    public function delete($id){
+
+        $comment = Comment::find($id);
+        return view('comments.delete')->withComment($comment);
+
     }
 
     /**
@@ -102,6 +90,12 @@ class CommentsController extends Controller
      */
     public function destroy($id)
     {
-        //
+       $comment = Comment::find($id);
+       $post_id = $comment->post->id;
+       $comment->delete();
+
+       Session::flash('success' , 'Delete Comment!');
+
+       return redirect()->route('posts.show', $post_id);  
     }
 }
