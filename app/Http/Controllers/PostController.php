c7fde7bfe90a3c6d598acfb72976d8aedbfdf9af
mindziukasss <mindziukasss@gmail.com>
2017-03-22 11:46:03 +0200

    Image upload for posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,7 @@ use App\Tag;
 use App\Category;
 use Session;
 use Purifier;
+use Image;
 
 class PostController extends Controller
 {
@@ -60,6 +61,15 @@ public function store(Request $request)
     $post->slug = $request->slug;
     $post->category_id = $request->category_id;
     $post->body = Purifier::clean($request->body);
+
+    if ($request->hasFile('featured_image')) {
+      $image = $request->file('featured_image');
+      $filename = time() . '.' . $image->getClientOriginalExtension();
+      $location = public_path('images/' . $filename);
+      Image::make($image)->resize(600, 300)->save($location);
+      $post->image = $filename;
+    }
+
     $post->save();
 
     $post->tags()->sync($request->tags, false); 
